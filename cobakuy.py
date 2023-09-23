fc3e40b32d35ebfa1df56a9904896cd34e35de49
adenurul13443 <91897439+adenurul13443@users.noreply.github.com>
2023-09-23 16:00:31 +0700

    Update cobakuy.py

--- cobakuy.py
+++ cobakuy.py
@@ -1,4 +1,7 @@
 from pycipher import Vigenere
+import streamlit as st
+
+st.title("Kriptografi")
 plain_text=input("Enter the message: ")
 key=input("Enter the key: ")
 vigenere_cipher = Vigenere (key).encipher(plain_text)
