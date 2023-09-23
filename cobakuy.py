80d7386eb871a8847703817e06d62e99c8d951d3
adenurul13443 <91897439+adenurul13443@users.noreply.github.com>
2023-09-23 15:59:30 +0700

    Add files via upload

---
+++ cobakuy.py
@@ -0,0 +1,13 @@
+from pycipher import Vigenere
+plain_text=input("Enter the message: ")
+key=input("Enter the key: ")
+vigenere_cipher = Vigenere (key).encipher(plain_text)
+print("---VIGENERE CIPHER---")
+print("CIPHER TEXT : ",vigenere_cipher)
+
+from pycipher import Beaufort
+beaufort_cipher = Beaufort (key).encipher(plain_text)
+d = Beaufort (key).decipher(beaufort_cipher)
+print("---BEAUFORT CIPHER---")
+print("CIPHER TEXT : ",beaufort_cipher)
+print("PLAIN TEXT : ",d)
