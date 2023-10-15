70a01a3a1618a446fe8e20324e494a36aefbd0c5
adenurul13443 <91897439+adenurul13443@users.noreply.github.com>
2023-10-15 09:43:39 +0700

    Update jurnal.py

--- jurnal.py
+++ jurnal.py
@@ -72,9 +72,12 @@ ber = calculate_ber(original_text, decrypted_text)
 cer = calculate_cer(original_text, decrypted_text)
 avalanche_effect = calculate_avalanche_effect(original_text, key)
 
-st.write(f'Teks Asli: {original_text}')
-st.write(f'Teks Terenkripsi: {cipher_text}')
-st.write(f'Teks Terdekripsi: {decrypted_text}')
-st.write(f"Bit Error Rate (BER): {ber:.4f}")
-st.write(f"Character Error Rate (CER): {cer:.4f}")
-st.write(f'Avalanche Effect: {avalanche_effect:.2f}%')
+
+if st.button('Enkripsi', type="primary"):
+    st.write('Why hello there')
+    st.write(f'Teks Asli: {original_text}')
+    st.write(f'Teks Terenkripsi: {cipher_text}')
+    st.write(f'Teks Terdekripsi: {decrypted_text}')
+    st.write(f"Bit Error Rate (BER): {ber:.4f}")
+    st.write(f"Character Error Rate (CER): {cer:.4f}")
+    st.write(f'Avalanche Effect: {avalanche_effect:.2f}%')
